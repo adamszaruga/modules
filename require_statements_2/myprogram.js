f7f09dcb0d3dadeabcc6a011cb1f50881bd0c7b1
RenteriaL <renterial@thesharepointprofessional.com>
2018-12-01 14:03:24 -0600

    statement 2

--- require_statements_2/myprogram.js
+++ require_statements_2/myprogram.js
@@ -2,6 +2,10 @@ const myModule1 = require('./file1')
 const myModule2 = require('./file2')
 const myModule3 = require('./file3')
 
-console.log(myModule1)
-console.log(myModule2)
-console.log(myModule3)
+var type1 = typeof myModule1
+var type2 = typeof myModule2
+var type3 = typeof myModule3
+
+console.log(type1)
+console.log(type2)
+console.log(type3)
