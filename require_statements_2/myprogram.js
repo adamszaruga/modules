915b83c2811280f22bbbf42b5d456622c39fa47f
RenteriaL <renterial@thesharepointprofessional.com>
2018-12-01 13:54:17 -0600

    pushing statements 2

---
+++ require_statements_2/myprogram.js
@@ -0,0 +1,7 @@
+const myModule1 = require('./file1')
+const myModule2 = require('./file2')
+const myModule3 = require('./file3')
+
+console.log(myModule1)
+console.log(myModule2)
+console.log(myModule3)
